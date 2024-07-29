607dafe7393360b91fd776c71b01cc0f5f22b74c
HoangEM-love <kimhoanganhem22112006@gmail.com>
2024-07-29 15:36:09 +0700

    index.html styles.css script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('yesButton').addEventListener('click', function() {
+    document.getElementById('response').innerText = "Tuyệt vời! Mình rất vui khi nghe điều này!";
+});
+
+document.getElementById('noButton').addEventListener('click', function() {
+    document.getElementById('response').innerText = "Không sao, mình vẫn sẽ luôn tôn trọng và quý mến bạn.";
+});
